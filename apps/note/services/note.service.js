66834a12cf673f1536a508f92debd7f50c9c97b7
liad7 <liadgola7@gmail.com>
2022-12-28 15:08:02 +0200

    create note service

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,78 @@
-console.log('Hi')
+import { storageService } from "../../../services/async-storage.service"
+import { utilService } from "../../../services/util.service"
+
+const NOTE_KEY = 'noteDB'
+
+_createNotes()
+
+export const noteService = {
+    query,
+    save,
+    get,
+    post,
+    remove,
+
+}
+
+
+function query() {
+    return storageService.query(NOTE_KEY)
+        .then(notes => notes)
+}
+
+function save(note) {
+    if (note.id) {
+        return storageService.put(NOTE_KEY, note)
+    } else {
+        return storageService.post(NOTE_KEY, note)
+    }
+}
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId)
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTE_KEY, noteId)
+}
+
+function post() {
+    // const note = _createnote(title, amount)
+    return storageService.post(NOTE_KEY, note)
+}
+
+function _createNotes() {
+    let notes = utilService.loadFromStorage(NOTE_KEY)
+    if (!notes || !notes.length) {
+        notes = [
+            {
+                id: "n101",
+                type: "note-txt",
+                isPinned: true,
+                info: {
+                    txt: "Fullstack Me Baby!"
+                }
+            }, {
+                id: "n103",
+                type: "note-todos",
+                info: {
+                    label: "Get my stuff together",
+                    todos: [
+                        { txt: "Driving liscence", doneAt: null },
+                        { txt: "Coding power", doneAt: 187111111 }]
+                }
+            }, {
+                id: "n102",
+                type: "note-img",
+                info: {
+                    url: "http://some-img/me",
+                    title: "Bobi and Me"
+                },
+                style: {
+                    backgroundColor: "#00d"
+                }
+            }
+        ]
+    }
+    utilService.saveToStorage(NOTE_KEY, notes)
+}
