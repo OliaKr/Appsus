76e71d51aa288fee4f4f870475fb11e39c00363f
liad7 <liadgola7@gmail.com>
2022-12-31 01:07:00 +0200

    main menu

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,6 +1,9 @@
+const { useState } = React
+
 const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
+    const [isMenuShown, setIsMenuShown] = useState(false)
 
     return <header className="app-header">
         <Link to="/">
@@ -11,11 +14,20 @@ export function AppHeader() {
             <input type="text"
                 placeholder="Search" />
         </form>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
+        <button onClick={() => setIsMenuShown(prev => !prev)}><i className="fa-solid fa-bars"></i></button>
+        {isMenuShown && <nav className="main-nav"
+            onClick={() => setIsMenuShown(prev => !prev)}>
+            {/* <NavLink to="/">Home</NavLink> */}
+            <NavLink to="/about"><i class="fa-solid fa-info"></i><h3>About</h3></NavLink>
+            <NavLink to="/mail"><i class="fa-solid fa-envelope"></i><h3>Mail</h3></NavLink>
+            <NavLink to="/note"><i class="fa-regular fa-note-sticky"></i><h3>Note</h3></NavLink>
+            <article><i class="fa-brands fa-youtube"></i><h3>Youtube</h3></article>
+            <article><i class="fa-regular fa-calendar"></i><h3>Calender</h3></article>
+            <article><i class="fa-solid fa-location-dot"></i><h3>Maps</h3></article>
+            <article><i class="fa-solid fa-earth-africa"></i><h3>Earth</h3></article>
+            <article><i class="fa-solid fa-user"></i><h3>Contacts</h3></article>
+            <article><i class="fa-solid fa-images"></i><h3>Photos</h3></article>
+
+        </nav>}
     </header>
 }
